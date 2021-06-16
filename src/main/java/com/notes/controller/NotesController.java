51b0e4faee47e66d620e20fd6a724c828aadad02
Gaurav Singh <grvsgh7@gmail.com>
2021-06-16 22:24:19 +0530

    initial commit

---
+++ src/main/java/com/notes/controller/NotesController.java
@@ -0,0 +1,27 @@
+package com.notes.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.notes.entity.Note;
+import com.notes.repository.NotesRepository;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class NotesController {
+	@Autowired
+	NotesRepository repo;
+	
+	@GetMapping("/notes")
+	public ResponseEntity<List<Note>> readNotes(){
+		return new ResponseEntity<List<Note>>(repo.findAll(), HttpStatus.OK);
+	}
+}
